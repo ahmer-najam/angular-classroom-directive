0e4c120d06032b921bdac89cced2c45cd3670080
Ahmer <ahmer-najam>
2023-04-17 08:54:13 +0500

    2023.04.17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { MyStrucDirectiveComponent } from './my-struc-directive/my-struc-directive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    MyStrucDirectiveComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-struc-directive/my-struc-directive.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-struc-directive',
+  templateUrl: './my-struc-directive.component.html',
+  styleUrls: ['./my-struc-directive.component.css']
+})
+export class MyStrucDirectiveComponent {
+
+}
